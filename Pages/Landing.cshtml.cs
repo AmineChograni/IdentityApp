a964f4215a260a3d6f43a04c98c221b2b149bb88
Amine Chograni <aminechograni@gmail.com>
2021-05-24 14:12:27 +0100

    Add project files.

---
+++ Pages/Landing.cshtml.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Pages
+{
+    public class LandingModel : PageModel
+    {
+        public LandingModel(ProductDbContext ctx) => DbContext = ctx;
+        public ProductDbContext DbContext { get; set; }
+    }
+}
